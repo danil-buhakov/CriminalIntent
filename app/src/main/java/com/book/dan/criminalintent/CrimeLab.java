d0fe899f5df56d7e1b018933ad70ff3669efa728
Danil Buhakov <tradinal@gmai.com>
2018-01-17 13:50:40 +0200

    Done recycler view with Crimes bining

---
+++ app/src/main/java/com/book/dan/criminalintent/CrimeLab.java
@@ -0,0 +1,42 @@
+package com.book.dan.criminalintent;
+
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context){
+        if(sCrimeLab==null){
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
+        for(int i = 0; i<100; i++){
+            Crime crime = new Crime();
+            crime.setSolved(i%2==0);
+            crime.setTitle("Crime №"+(i+1));
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes(){
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for(Crime c:mCrimes){
+            if(c.getId().equals(id))
+                return c;
+        }
+        return null;
+    }
+}
