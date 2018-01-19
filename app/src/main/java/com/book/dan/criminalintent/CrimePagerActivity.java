5b107f2eef357117c18b5ef2026be6a878dcd5a6
Danil Buhakov <tradinal@gmai.com>
2018-01-19 15:27:01 +0200

    Added to first and to last buttons

--- app/src/main/java/com/book/dan/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/book/dan/criminalintent/CrimePagerActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +19,8 @@ public class CrimePagerActivity extends AppCompatActivity {
     private static final String EXTRA_CRIME_ID = "id";
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+    private Button mButtonFirst;
+    private Button mButtonLast;
 
 
     @Override
@@ -27,13 +31,29 @@ public class CrimePagerActivity extends AppCompatActivity {
         UUID crimeId = (UUID) getIntent()
                 .getSerializableExtra(EXTRA_CRIME_ID);
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+        mButtonFirst = (Button) findViewById(R.id.btn_first);
+        mButtonLast = (Button) findViewById(R.id.btn_last);
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fm = getSupportFragmentManager();
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
             @Override
             public Fragment getItem(int position) {
-                Crime currCrime = mCrimes.get(position);
-                return CrimeFragment.newInstance(currCrime.getId());
+                int curr = mViewPager.getCurrentItem();
+                if(curr==0) {
+                    mButtonFirst.setEnabled(false);
+                    mButtonLast.setEnabled(true);
+                }
+                else
+                    if(curr==(mCrimes.size()-1)) {
+                        mButtonFirst.setEnabled(true);
+                        mButtonLast.setEnabled(false);
+                    }
+                    else{
+                        mButtonFirst.setEnabled(true);
+                        mButtonLast.setEnabled(true);
+                    }
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
             }
 
             @Override
@@ -43,10 +63,26 @@ public class CrimePagerActivity extends AppCompatActivity {
         });
         for(int i=0; i<mCrimes.size(); i++){
             if(mCrimes.get(i).getId().equals(crimeId)){
+                if(i==0)
+                    mButtonFirst.setEnabled(false);
+                if(i==(mCrimes.size()-1))
+                    mButtonLast.setEnabled(false);
                 mViewPager.setCurrentItem(i);
                 break;
             }
         }
+        mButtonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+        mButtonLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size());
+            }
+        });
     }
 
     public static Intent newIntent(Context context, UUID id){
