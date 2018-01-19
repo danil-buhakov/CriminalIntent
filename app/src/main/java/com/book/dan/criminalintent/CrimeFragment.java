21d6dfc1dfb608922f3b22c64e6908c1f7fd9313
Danil Buhakov <tradinal@gmai.com>
2018-01-19 16:16:51 +0200

    Added dialog

--- app/src/main/java/com/book/dan/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/book/dan/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ package com.book.dan.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -18,6 +19,7 @@ import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
     private static final String ARG_CRIME_ID="crime_id";
+    private static final String DIALOG_DATE = "Dialog date";
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
@@ -61,7 +63,14 @@ public class CrimeFragment extends Fragment {
         });
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                DatePickerFragment fragment = new DatePickerFragment();
+                fragment.show(fm,DIALOG_DATE);
+            }
+        });
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
