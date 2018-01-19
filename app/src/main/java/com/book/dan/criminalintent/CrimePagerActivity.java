59684887e4184fe5b7c825f12b14d832d502097d
Danil Buhakov <tradinal@gmai.com>
2018-01-19 15:03:20 +0200

    Added viewPagerActivity and view pager

---
+++ app/src/main/java/com/book/dan/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,56 @@
+package com.book.dan.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID = "id";
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime currCrime = mCrimes.get(position);
+                return CrimeFragment.newInstance(currCrime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+        for(int i=0; i<mCrimes.size(); i++){
+            if(mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+    }
+
+    public static Intent newIntent(Context context, UUID id){
+        Intent intent = new Intent(context, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID,id);
+        return intent;
+    }
+}
